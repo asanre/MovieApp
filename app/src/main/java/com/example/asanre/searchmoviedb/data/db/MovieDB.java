4a6645aab88abd925d92570360f89bc2ef323b00
asserr <antoni.sanchez@berepublic.es>
2018-01-18 11:28:28 +0100

    Add room db for top movie list

---
+++ app/src/main/java/com/example/asanre/searchmoviedb/data/db/MovieDB.java
@@ -0,0 +1,30 @@
+package com.example.asanre.searchmoviedb.data.db;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+import com.example.asanre.searchmoviedb.data.model.MovieEntity;
+
+@Database(entities = {MovieEntity.class}, version = 1)
+public abstract class MovieDB extends RoomDatabase {
+
+    public static final String DATABASE_NAME = "movies";
+    private static MovieDB instance;
+
+    public static MovieDB getInstance(Context context) {
+
+        if (instance == null) {
+            synchronized (MovieDB.class) {
+                if (instance == null) {
+                    instance = Room.databaseBuilder(context.getApplicationContext(), MovieDB.class,
+                            MovieDB.DATABASE_NAME).build();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public abstract MovieDao getMovieDao();
+}
---
+++ app/src/main/java/com/example/asanre/searchmoviedb/data/db/MovieDao.java
@@ -0,0 +1,25 @@
+package com.example.asanre.searchmoviedb.data.db;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+
+import com.example.asanre.searchmoviedb.data.model.MovieEntity;
+
+import java.util.List;
+
+import io.reactivex.Single;
+
+@Dao
+public interface MovieDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void save(MovieEntity... movie);
+
+    @Query("SELECT * FROM movie WHERE page = :page")
+    Single<List<MovieEntity>> getMoviesByPage(int page);
+
+    @Query("delete from movie")
+    void clear();
+}
