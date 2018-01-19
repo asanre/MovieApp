ce53bd0e8de15ca25b9bde63bc423a1edac44bb6
asserr <antoni.sanchez@berepublic.es>
2018-01-19 12:14:19 +0100

    Add composite disposable to presenter

--- app/src/main/java/com/example/asanre/searchmoviedb/ui/base/BasePresenter.java
+++ app/src/main/java/com/example/asanre/searchmoviedb/ui/base/BasePresenter.java
@@ -1,11 +1,37 @@
 package com.example.asanre.searchmoviedb.ui.base;
 
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+
 public abstract class BasePresenter {
+
+    public CompositeDisposable mCompositeDisposable = new CompositeDisposable();
 
     public abstract BaseView getView();
 
     public boolean isViewAlive() {
 
         return getView() != null && getView().isViewAlive();
+    }
+
+    public void addDisposable(Disposable disposable) {
+
+        if (disposable != null && mCompositeDisposable != null) {
+            mCompositeDisposable.add(disposable);
+        }
+    }
+
+    public void clear() {
+
+        if (mCompositeDisposable != null) {
+            mCompositeDisposable.clear();
+        }
+    }
+
+    public void dispose() {
+
+        if (mCompositeDisposable != null && !mCompositeDisposable.isDisposed()) {
+            mCompositeDisposable.dispose();
+        }
     }
 }
